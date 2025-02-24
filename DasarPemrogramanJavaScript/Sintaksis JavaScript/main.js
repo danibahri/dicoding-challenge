5128795b8b905da42ce043ef697984c4a203c05b
danibahri <danibahri45@gmail.com>
2025-02-24 19:13:36 +0700

    javascript

---
+++ DasarPemrogramanJavaScript/Sintaksis JavaScript/main.js
@@ -0,0 +1,10 @@
+// statements $ expressions
+const age = 19;
+const name = "John";
+console.log(name + " is " + age + " years old.");
+
+// let user = "John";
+// console.log(user);
+
+user = "Jane";
+console.log(user);
