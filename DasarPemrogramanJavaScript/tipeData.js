9327cb42247b94532c7bc93ac0900d32ff3ce7c6
danibahri <danibahri45@gmail.com>
2025-03-08 11:01:57 +0700

    update

---
+++ DasarPemrogramanJavaScript/tipeData.js
@@ -0,0 +1,34 @@
+// String
+var nama = "Jhon Doe";
+console.log(nama);
+
+// Number
+var umur = 19;
+console.log(umur);
+
+// Boolean
+var isMarried = false;
+console.log(isMarried);
+
+// Undefined
+var alamat;
+console.log(alamat);
+
+// Null
+var jenkel = null;
+console.log(jenkel);
+
+// Array
+var hobi = ["Travelling", "Reading"];
+console.log(hobi);
+
+// Object
+var orang = {
+  nama: "Jhon Doe",
+  umur: 19,
+  isMarried: false,
+  alamat: "Jakarta",
+};
+
+console.log(orang);
+console.log(orang.nama);
