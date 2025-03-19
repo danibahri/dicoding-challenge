7378e201244ca24eb896006a0e59532ae481ff5c
danibahri <danibahri45@gmail.com>
2025-03-19 09:02:53 +0700

    update

---
+++ DasarPemrogramanJavaScript/ekspression.js
@@ -0,0 +1,51 @@
+// Function Ekspresi
+// Function expression adalah cara lain untuk mendefinisikan fungsi.
+const convertCelsiusToFahrenheit = function (temperature) {
+  const result = (9 / 5) * temperature + 32;
+  return result;
+};
+
+const temperatureInFahrenheit = convertCelsiusToFahrenheit(90);
+console.log("Hasil konversi:", temperatureInFahrenheit);
+
+/**
+ * Output:
+ * Hasil konversi: 194
+ */
+
+// First-Class Citizen
+/**
+ * Jika ada bahasa pemrograman yang mengatakan bahwa function dapat diperlakukan layaknya variabel,
+ * function tersebut dinyatakan sebagai first-class citizen. Apa maksud dari diperlakukan mirip variabel?
+ *
+ * Pada JavaScript, function dapat kita jadikan sebagai nilai dan disimpan dalam variabel,
+ * nilai argumen function lain, mengembalikan nilai function dari suatu function, dsb. Mari kita lihat contohnya.
+ */
+function multiply(a, b) {
+  return a * b;
+}
+
+function calculate(operation, numA, numB) {
+  return operation(numA, numB);
+}
+
+const result = calculate(multiply, 2, 4);
+console.log(result);
+// Output: 8
+
+function multiplier(x) {
+  return function (num) {
+    return x * num;
+  };
+}
+
+const double = multiplier(2);
+const triple = multiplier(3);
+
+console.log(double(10));
+console.log(triple(11));
+
+/* Output:
+  20
+  33
+  */
--- DasarPemrogramanJavaScript/parameter.js
+++ DasarPemrogramanJavaScript/parameter.js
@@ -24,3 +24,45 @@ convertCelsiusToFahrenheit(100);
 // Memasukkan nilai dengan variabel
 const InCelsius = 90;
 convertCelsiusToFahrenheit(InCelsius);
+
+// Default Parameter
+/**
+ * Tahukah Anda bahwa argument dapat bernilai undefined jika kita tidak beri nilai apa pun dalam parentheses saat function dipanggil?
+ * Lalu, apa yang akan terjadi jika function body tetap dijalankan dalam keadaan seperti itu? Mari kita lihat bersama.
+ */
+function convertCelsiusToFahrenheit(temperature) {
+  const temperatureInFahrenheit = (9 / 5) * temperature + 32;
+  console.log("Hasil konversi:", temperatureInFahrenheit);
+}
+
+convertCelsiusToFahrenheit();
+
+/* Output:
+  Hasil konversi: NaN
+  */
+
+function convertCelsiusToFahrenheit(temperature = 50) {
+  const temperatureInFahrenheit = (9 / 5) * temperature + 32;
+
+  console.log("Hasil konversi:", temperatureInFahrenheit);
+}
+
+// Menggunakan default value
+convertCelsiusToFahrenheit(); // output: Hasil konversi: 122
+
+// Menggantikan default value
+
+// Return Value
+/**
+ * Function dapat menerima input dan mengembalikan output.
+ * Function didefinisikan dengan kata kunci function diikuti oleh nama fungsi dan tanda kurung ().
+ */
+
+function convertCelsiusToFahrenheit(temperature) {
+  const temperatureInFahrenheit = (9 / 5) * temperature + 32;
+  return temperatureInFahrenheit;
+}
+
+const celsius = 95;
+const result = convertCelsiusToFahrenheit(celsius);
+console.log("Hasil konversi:", result);
