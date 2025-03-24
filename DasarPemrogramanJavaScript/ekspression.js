0250e7a23cc42c4254850a3738d3b684e1f4ce75
danibahri <danibahri45@gmail.com>
2025-03-24 21:18:00 +0700

    update

--- DasarPemrogramanJavaScript/ekspression.js
+++ DasarPemrogramanJavaScript/ekspression.js
@@ -1,76 +1,76 @@
-// // Function Ekspresi
-// // Function expression adalah cara lain untuk mendefinisikan fungsi.
-// const convertCelsiusToFahrenheit = function (temperature) {
-//   const result = (9 / 5) * temperature + 32;
-//   return result;
-// };
+// Function Ekspresi
+// Function expression adalah cara lain untuk mendefinisikan fungsi.
+const convertCelsiusToFahrenheit = function (temperature) {
+  const result = (9 / 5) * temperature + 32;
+  return result;
+};
 
-// const temperatureInFahrenheit = convertCelsiusToFahrenheit(90);
-// console.log("Hasil konversi:", temperatureInFahrenheit);
+const temperatureInFahrenheit = convertCelsiusToFahrenheit(90);
+console.log("Hasil konversi:", temperatureInFahrenheit);
 
-// /**
-//  * Output:
-//  * Hasil konversi: 194
-//  */
+/**
+ * Output:
+ * Hasil konversi: 194
+ */
 
-// // First-Class Citizen
-// /**
-//  * Jika ada bahasa pemrograman yang mengatakan bahwa function dapat diperlakukan layaknya variabel,
-//  * function tersebut dinyatakan sebagai first-class citizen. Apa maksud dari diperlakukan mirip variabel?
-//  *
-//  * Pada JavaScript, function dapat kita jadikan sebagai nilai dan disimpan dalam variabel,
-//  * nilai argumen function lain, mengembalikan nilai function dari suatu function, dsb. Mari kita lihat contohnya.
-//  */
-// function multiply(a, b) {
-//   return a * b;
-// }
+// First-Class Citizen
+/**
+ * Jika ada bahasa pemrograman yang mengatakan bahwa function dapat diperlakukan layaknya variabel,
+ * function tersebut dinyatakan sebagai first-class citizen. Apa maksud dari diperlakukan mirip variabel?
+ *
+ * Pada JavaScript, function dapat kita jadikan sebagai nilai dan disimpan dalam variabel,
+ * nilai argumen function lain, mengembalikan nilai function dari suatu function, dsb. Mari kita lihat contohnya.
+ */
+function multiply(a, b) {
+  return a * b;
+}
 
-// function calculate(operation, numA, numB) {
-//   return operation(numA, numB);
-// }
+function calculate(operation, numA, numB) {
+  return operation(numA, numB);
+}
 
-// const result = calculate(multiply, 2, 4);
-// console.log(result);
-// // Output: 8
+const result = calculate(multiply, 2, 4);
+console.log(result);
+// Output: 8
 
-// function multiplier(x) {
-//   return function (num) {
-//     return x * num;
-//   };
-// }
+function multiplier(x) {
+  return function (num) {
+    return x * num;
+  };
+}
 
-// const double = multiplier(2);
-// const triple = multiplier(3);
+const double = multiplier(2);
+const triple = multiplier(3);
 
-// console.log(double(10));
-// console.log(triple(11));
+console.log(double(10));
+console.log(triple(11));
 
-// /* Output:
-//   20
-//   33
-//   */
+/* Output:
+  20
+  33
+  */
 
-// //Arrow function
-// /**
-//  * Arrow function adalah cara lain untuk mendefinisikan fungsi.
-//  * Arrow function diperkenalkan pada ES6.
-//  * Arrow function lebih ringkas dan tidak memerlukan kata kunci function.
-//  */
-// const greet = () => {
-//   console.log("Halo!");
-// };
+//Arrow function
+/**
+ * Arrow function adalah cara lain untuk mendefinisikan fungsi.
+ * Arrow function diperkenalkan pada ES6.
+ * Arrow function lebih ringkas dan tidak memerlukan kata kunci function.
+ */
+const greet = () => {
+  console.log("Halo!");
+};
 
-// greet();
+greet();
 
-// // Deklarasi Function dengan Arrow Function
-// const convert = (temperature) => {
-//   const result = (9 / 5) * temperature + 32;
-//   return result;
-// };
+// Deklarasi Function dengan Arrow Function
+const convert = (temperature) => {
+  const result = (9 / 5) * temperature + 32;
+  return result;
+};
 
-// InFahrenheit = convert(90);
-// console.log("Hasil konversi:", InFahrenheit);
-// // Hasil ==> Hasil konversi: 194
+InFahrenheit = convert(90);
+console.log("Hasil konversi:", InFahrenheit);
+// Hasil ==> Hasil konversi: 194
 
 let a = null;
 
@@ -80,4 +80,39 @@ const convertToKelvin = (celsius) => {
 };
 
 a = convertToKelvin(27);
-console.log("Hasil konversi:", a);
+console.log("Hasil konversi  ke Kelvin:", a);
+
+const convertToReamur = function (celsius) {
+  const result = (4 / 5) * celsius;
+  return result;
+};
+
+a = convertToReamur(27);
+console.log("Hasil konversi Reamur:", a);
+
+const greeto = (a = "hallo nama saya dani ") => {
+  console.log(a);
+};
+
+greeto();
+
+let temperatureToFahrenheit = null;
+
+// Arrow function
+// const convertCelsiusToFahrenheit = (temperature) => {
+//   const result = (9 / 5) * temperature + 32;
+//   return result;
+// };
+
+temperatureToFahrenheit = convertCelsiusToFahrenheit(90);
+console.log("Hasil konversi:", temperatureToFahrenheit);
+
+// Arrow function versi ringkas
+const convertCelsiusToFahrenheitInConciseSyntax = (temperature) =>
+  (9 / 5) * temperature + 32;
+
+temperatureToFahrenheit = convertCelsiusToFahrenheitInConciseSyntax(90);
+console.log("Hasil konversi:", temperatureToFahrenheit);
+
+const greeter = () => "Halo!";
+console.log(greeter());
---
+++ DasarPemrogramanJavaScript/object.js
@@ -0,0 +1,10 @@
+// const user = {};
+const user = {
+  name: "Dicoding",
+  "last name": "Indonesia",
+  age: 9,
+};
+const product = { name: "pen", price: 2.5 };
+
+console.log(user);
+console.log(product["name"]);
