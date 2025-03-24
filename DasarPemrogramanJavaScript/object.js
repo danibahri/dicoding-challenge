eed951a5b0dfa7495484e7009fc13c8e127ff5f3
danibahri <danibahri45@gmail.com>
2025-03-25 03:53:03 +0700

    update

--- DasarPemrogramanJavaScript/object.js
+++ DasarPemrogramanJavaScript/object.js
@@ -1,11 +1,29 @@
-// const user = {};
-const user = {
-  name: "Dicoding",
-  "last name": "Indonesia",
-  age: 9,
-};
-const product = { name: "pen", price: 2.5 };
+// // const user = {};
+// const user = {
+//   name: "Dicoding",
+//   lastName: "Indonesia",
+//   age: 9,
+// };
+// const product = { name: "pen", price: 2.5 };
 
-console.log(user);
-console.log("Menngunakan dot :", product.name);
-console.log("Menggunakan Bracket :", product["name"]);
+// console.log(user);
+// console.log("Menngunakan dot :", product.name);
+// console.log("Menggunakan Bracket :", product["name"]);
+
+// // const { name, lastName } = user;
+// // console.log(name, lastName); // Output: Dicoding Indonesia
+// const { name, lastName, isMale = false } = user;
+// console.log(isMale); // Output: false
+
+const user = {
+  id: 24,
+  email: "aras@dicoding.com",
+  name: "Arsy",
+  nickname: "Aras",
+  username: "aras123",
+  password: "secret",
+};
+
+const { id, email, name } = user;
+
+console.log(`ID : ${id}, Email : ${email}, Nama : ${name}`);
